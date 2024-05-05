792c532e34305fb2b6a2228aead0c884caf3fbf1
BurakYs <brkyhs0@gmail.com>
2024-05-05 14:33:34 +0300

    Custom swagger theme

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -2,6 +2,7 @@ import { SwaggerOptions } from '@fastify/swagger';
 import { FastifySwaggerUiOptions } from '@fastify/swagger-ui';
 import { jsonSchemaTransform } from 'fastify-type-provider-zod';
 import appConfig from '@/config/app';
+import fs from 'fs';
 
 export const swaggerConfig: SwaggerOptions = {
     openapi: {
@@ -13,7 +14,7 @@ export const swaggerConfig: SwaggerOptions = {
         },
         servers: [
             {
-                url: appConfig.app.url
+                url: appConfig.app.url,
             }
         ],
         tags: [
@@ -29,5 +30,10 @@ export const swaggerUIConfig: FastifySwaggerUiOptions = {
         docExpansion: 'list',
         deepLinking: false,
         layout: 'BaseLayout'
+    },
+    theme: {
+        title: 'Fastify API Documentation',
+        css: [{ filename: 'swagger.css', content: fs.readFileSync('./public/swagger.css', 'utf-8') }],
+        favicon: [{ filename: 'favicon.ico', rel: 'icon', type: 'image/x-icon', sizes: '16x16', content: fs.readFileSync('./public/favicon.ico') }]
     }
 };
