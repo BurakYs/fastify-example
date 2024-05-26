ab01efef51b03500b28058af7dd481059a406f43
BurakYs <brkyhs0@gmail.com>
2024-05-26 15:39:24 +0300

    remove unnecessary return

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import 'module-alias/register';
 import 'dotenv/config';
 import { Logger } from '@/helpers';
 import Server from './server';
-import { existsSync, mkdirSync } from 'fs';
-
-if (!existsSync('./logs')) mkdirSync('./logs');
 
 if (process.argv.includes('--production')) process.env.NODE_ENV = 'production';
 
