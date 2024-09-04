4e0b2528431c0bbc90e4e46d912122b892a02dd7
BurakYs <brkyhs0@gmail.com>
2024-09-04 20:17:28 +0300

    Fix app config casing

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,9 +2,9 @@ const { NODE_ENV = '', PORT = 3000 } = process.env;
 
 const isProduction = ['prod', 'production'].includes(NODE_ENV.toLowerCase());
 
-const AppConfig = {
+const appConfig = {
     rootUrl: isProduction ? 'https://api.example.com' : `http://localhost:${PORT}`,
     isProduction
 };
 
-export default AppConfig;
+export default appConfig;
