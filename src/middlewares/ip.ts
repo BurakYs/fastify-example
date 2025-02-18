2f2e6ada2d61fed77b57fd7dc402eab9fe6f68c9
BurakYs <brkyhs0@gmail.com>
2025-02-18 23:34:08 +0300

    Fix ip middleware

--- src/middlewares/ip.ts
+++ src/middlewares/ip.ts
@@ -1,6 +1,6 @@
 import type { FastifyRequest } from 'fastify';
 
 export default function ip(request: FastifyRequest) {
-    const ip = (request.headers['x-forwarded-for'] || request.ip || '127.0.0.1') as string;
-    return ip.split(',').at(-1);
+    const proxyIp = (request.headers['x-forwarded-for'] as string | undefined)?.split(',').at(-1);
+    return proxyIp || request.ip;
 }
