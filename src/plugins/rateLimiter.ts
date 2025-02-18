da8bbe5ad2c0251440b343819786dd733ee3491e
BurakYs <brkyhs0@gmail.com>
2025-02-18 23:37:52 +0300

    Remove 127.0.0.1 from rate limit allow list

--- src/plugins/rateLimiter.ts
+++ src/plugins/rateLimiter.ts
@@ -7,7 +7,6 @@ export default fp(async (fastify) => {
         max: 50,
         timeWindow: 60000,
         hook: 'preParsing',
-        allowList: ['127.0.0.1'],
         keyGenerator: (request) => request.clientIp,
         skipOnError: false
     } satisfies RateLimitPluginOptions);
