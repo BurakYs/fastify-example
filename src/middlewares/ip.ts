11dcd915c30943974b799ce70cc723ecce40e96a
BurakYs <brkyhs0@gmail.com>
2025-02-18 23:36:09 +0300

    changes

--- src/middlewares/ip.ts
+++ src/middlewares/ip.ts
@@ -2,5 +2,7 @@ import type { FastifyRequest } from 'fastify';
 
 export default function ip(request: FastifyRequest) {
     const proxyIp = (request.headers['x-forwarded-for'] as string | undefined)?.split(',').at(-1);
+
+    request.clientIp = proxyIp || request.ip;
     return proxyIp || request.ip;
 }
