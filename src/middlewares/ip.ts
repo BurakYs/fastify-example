fb59b3d5865b01d46ff29a1a1b64fba2ae4e2a96
BurakYs <brkyhs0@gmail.com>
2025-02-18 23:29:02 +0300

    Handle IPs in local requests

--- src/middlewares/ip.ts
+++ src/middlewares/ip.ts
@@ -1,6 +1,6 @@
 import type { FastifyRequest } from 'fastify';
 
 export default function ip(request: FastifyRequest) {
-    const ip = (request.headers['x-forwarded-for'] || request.ip) as string;
+    const ip = (request.headers['x-forwarded-for'] || request.ip || '127.0.0.1') as string;
     return ip.split(',').at(-1);
 }
